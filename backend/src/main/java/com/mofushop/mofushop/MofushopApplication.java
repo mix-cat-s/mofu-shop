5d5c946fcf356b28db5a1eb788640f069a88d3d3
Rouh <roaming.new@gmail.com>
2024-02-18 16:09:31 +0900

    backendのプロジェクトを追加

---
+++ backend/src/main/java/com/mofushop/mofushop/MofushopApplication.java
@@ -0,0 +1,13 @@
+package com.mofushop.mofushop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MofushopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MofushopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mofushop/mofushop/MofushopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mofushop.mofushop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MofushopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
