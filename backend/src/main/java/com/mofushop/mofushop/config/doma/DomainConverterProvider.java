fa13f8540d73f13aa65b87d4907acf822039e464
混猫。 <46620955+mazeneko@users.noreply.github.com>
2024-03-12 00:22:22 +0900

    mazewandsのQueryとCommandを使うように変更

--- backend/src/main/java/com/mofushop/mofushop/config/doma/DomainConverterProvider.java
+++ backend/src/main/java/com/mofushop/mofushop/config/doma/DomainConverterProvider.java
@@ -3,6 +3,7 @@ package com.mofushop.mofushop.config.doma;
 import org.seasar.doma.DomainConverters;
 
 @DomainConverters({
+    IdentifierConverter.class,
     UUIDConverter.class,
 })
 public class DomainConverterProvider {
